25314b0e896e6922f7b66ee4f05344a010eed87b
Kseniya <email@email>
2023-07-03 23:23:28 +0300

    code: kinda done probably

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <ctime>
+#include <chrono>
+
 #include <curl/curl.h>
 
 using namespace std;
@@ -20,7 +23,7 @@ vector<double> numbers;
 size_t bin_count;
 };
 
-vector<double> input_numbers(ifstream& in, size_t count) {
+vector<double> input_numbers(istream& in, size_t count) {
     in >> count;
     vector<double> result(count);
     for (size_t i = 0; i < count; i++) {
@@ -29,7 +32,7 @@ vector<double> input_numbers(ifstream& in, size_t count) {
     return result;
 }
 
-Input read_input(ifstream& in) {
+Input read_input(istream& in) {
 Input data;
 size_t number_count;
 in >> number_count;
@@ -185,11 +188,25 @@ if(curl) {
        if (res) {
         // res = 0 is OK
         cout << "\n" << curl_easy_strerror(res) << "\n";
+        char *ip;
+        auto start = chrono::steady_clock::now();
+
+        curl_easy_getinfo(curl, CURLINFO_PRIMARY_IP, &ip);
+        auto end = chrono::steady_clock::now();
+           auto diff = end - start;
+       cout << chrono::duration <double, milli>(diff).count()  << "\n";
+
         exit(1);
 
        }
 }
-return read_input(buffer, false);
+return read_input(buffer);
+}
+
+size_t
+write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+buffer.write(items, item_size * item_count);
+return 0;
 }
 
 int main(int argc, char* argv[])
@@ -210,4 +227,9 @@ input = read_input(cin, true);
     Input data = read_input(file);
     auto bins = make_histogram(data.numbers, data.bin_count);
     show_histogram_svg(bins, "1.svg");
+
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    const char *items = reinterpret_cast<const char*>(ctx);
+    buffer->write(items, data_size);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
 }
